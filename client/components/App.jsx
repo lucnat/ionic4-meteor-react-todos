1e5c4690884b872cf85efce7d81bdc919a59306f
Luca Naterop <luca.naterop@bluewin.ch>
2018-09-05 13:29:12 +0200

    latest css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,9 +15,9 @@ export default class App extends React.Component {
     return (
       <ion-fab 
         onClick={this.onAdd.bind(this)}
-        style={{bottom: 25, left: 25}} 
+        style={{bottom: 25, right: 25}} 
         slot="fixed" class="fab-horizontal-end fab-vertical-bottom fab-edge hydrated">
-        <ion-fab-button ion-activable="" class="hydrated activated">
+        <ion-fab-button ion-activable="" class="ion-color-dark hydrated activated">
           <ion-icon name="add" style={{fontSize: 30}}></ion-icon>
         </ion-fab-button>
       </ion-fab>
@@ -26,19 +26,17 @@ export default class App extends React.Component {
   
   render() {
     return (
-      <ion-app mode="ios">
-        <ion-header translucent>
-          <ion-toolbar color="secondary">
-            <ion-buttons slot="start" class="sc-ion-buttons-ios-h sc-ion-buttons-ios-s hydrated">
-              <ion-menu-button class="button hydrated"></ion-menu-button>
-            </ion-buttons>
+      <ion-app>
+        <ion-header>
+          <ion-toolbar>
             <ion-title>Todos</ion-title>
           </ion-toolbar>
         </ion-header>
-        <div>
+
+        <ion-content style={{backgroundColor: 'transparent'}}>
           {this.renderFab()}
           <TodoList />
-        </div>
+        </ion-content>
       </ion-app>
     ); 
   }
