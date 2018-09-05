47e55e8f57701374b0052e90ecadf1b4c6e48be4
Luca <luca.naterop@bluewin.ch>
2018-09-05 11:38:19 +0200

    fab

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,23 @@ import React from 'react';
 import TodoList from './TodoList';
 
 export default class App extends React.Component {
+
+  onAdd() {
+    console.log('yeah');
+  }
+
+  renderFab() {
+    return (
+      <ion-fab 
+        onClick={this.onAdd.bind(this)}
+        style={{bottom: 25, left: 25}} 
+        slot="fixed" class="fab-horizontal-end fab-vertical-bottom fab-edge hydrated">
+        <ion-fab-button ion-activable="" class="hydrated activated">
+          <ion-icon name="add" style={{fontSize: 30}}></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    );
+  }
   
   render() {
     return (
@@ -19,6 +36,7 @@ export default class App extends React.Component {
           </ion-toolbar>
         </ion-header>
         <div>
+          {this.renderFab()}
           <TodoList />
         </div>
       </ion-app>
